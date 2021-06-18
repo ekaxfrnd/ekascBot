db9aa4b0b5cc6a3ae7eb06d334e560e32137db70
ekaxfrnd <ekaxfrnd@gmail.com>
2021-06-18 20:38:56 +0800

    no comment

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config()
+const fs = require('fs')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start(ctx => {
-    ctx.reply('haii')
+    ctx.repl
+    y('haii')
+})
+
+bot.command('monitor', ctx => {
+    fs.watchFile('snort.log', (curr, prev) => {
+        console.log(`the current mtime is: ${curr.mtime}`)
+        console.log(`the previous mtime was: ${prev.mtime}`)
+    })
 })
 
 bot.launch()
